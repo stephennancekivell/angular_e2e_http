8c34fe0e87381e7f146bd48284e2a5c21f4b0ad6
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-04-14 18:39:10 +1200

    fix e2e test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,15 +5,15 @@
 describe('my app', function() {
 
   beforeEach(function() {
-    browser().navigateTo('../../app/index.html#/view1');
+    browser().navigateTo('../../app/index.test.html#/view1');
   });
 
   describe('view1', function() {
 
     it('should list some things that it got from the server', function() {
       // Here we know what the things are because they are in mocks.js.
-      expect(repeater('ul.things li').count()).toBe(3);
-      expect(element('ul.things li').text()).toEqual('AppleBannanaOrange');
+      expect(repeater('ul li').count()).toBe(3);
+      expect(element('ul li').text()).toEqual('AppleBannanaOrange');
     });
 
   });
