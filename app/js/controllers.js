a604fbf167a01ab687241db284dc58c62bb0d612
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-04-14 16:46:22 +1200

    delete stuff not core to the example

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,5 @@
 angular.module('myApp.controllers', ['ngResource']).
   controller('MyCtrl1', ['$resource', '$scope', function($resource, $scope) {
     $scope.things = $resource('/things', {}, {}).get();
-
-  }])
-  .controller('MyCtrl2', [function() {
-
   }]);
+ 
