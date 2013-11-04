867144a0953713ec7099a5418a4582ea342c69f1
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-11-04 21:32:04 +1300

    tidy
    add jquery

--- testServer.js
+++ testServer.js
@@ -7,23 +7,20 @@ var route = router();
 var toRespond = [];
 
 route.post('/things', function(req, res) {
-	// console.log(req);
 	req.on('data', function(chunk) {
 		toRespond.push(chunk);
 		console.log('chunk' + chunk);
-	})
+	});
 	res.writeHead(200);
 	res.end();
 });
 
 route.get('/things', function(req, res) {
-	// res.setHeader("Content-Type", "application/json");
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With');
 	res.writeHead(200);
 	res.end(toRespond.pop());
-	// res.end('[{"name":"Apple"},{"name":"Bannana"},{"name":"Orange"}]');
 	console.log('.');
 });
 
@@ -37,4 +34,4 @@ route.options('/things', function(req, res) {
 	res.end();
 });
 
-http.createServer(route).listen(9001); // start the server on port 8080
+http.createServer(route).listen(9001);
